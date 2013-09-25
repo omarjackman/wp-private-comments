9f3ce7092dd1e225139c6022545b51f4202209a6
Omar Jackman <git@omarjackman.com>
2013-09-25 14:03:39 -0400

    Added filtering logic I added a filter for comments_array which strips out comments that should not be visible to the current user. I also added a filter for get_comments_number which adjusts the number displayed for the comment count

--- lib/WP_PrivateComments.class.php
+++ lib/WP_PrivateComments.class.php
@@ -6,6 +6,7 @@
 	class WP_PrivateComments {
 
 		protected static $_instance;
+		protected $_comment_counts = array();
 
 		const FIELD_PREFIX = 'wp-private-comment-';
 		const VISIBILITY_EVERYONE = '';
@@ -33,6 +34,10 @@
 			add_action( 'comment_post', array($this, 'comment_post') );
 			add_filter( 'comment_form_default_fields', array($this, 'comment_form_default_fields') );
 			add_filter( 'comment_form_logged_in', array($this, 'comment_form_logged_in') );
+			add_filter( 'comments_array', array($this, 'comments_array'), 10, 2 );
+			add_filter( 'get_comments_number', array($this, 'get_comments_number'), 10, 2 );
+			
+			//TODO: Add filter that will remove hidden comments from feeds
 		}
 
 		/**
@@ -64,6 +69,92 @@
 			return apply_filters('WP_PrivateComments::getFields', array(
 				'visibility' => '<p class="comment-form-visibility"><label for="visibility">' . __( 'Visibility' ) . '</label><select id="visibility" name="visibility"/>' . $options . '</select>'
 			));
+		}
+
+		function get_comments_number($count, $post_id){
+			if(isset($this->_comment_counts[$post_id]))
+				return $this->_comment_counts[$post_id];
+			else
+				return $count;
+		}
+
+		/**
+		 * Filter an array of comment objects and remove comments that should not be visible for the logged in user
+		 * @param array $comments 
+		 * @return array
+		 */
+		function comments_array($comments, $post_id){			
+			global $wpdb, $current_user, $post;
+
+			$filtered_comments = array();
+			
+			$comment_ids = array();
+			
+			foreach($comments as $comment){
+				$comment_ids[] = intval($comment->comment_ID);
+			}
+
+			$comment_ids = implode(',', $comment_ids);
+
+			$current_user_id = intval($current_user->ID);
+
+			$commenter = wp_get_current_commenter();
+			$comment_author = $commenter['comment_author'];
+			$comment_author_email = $commenter['comment_author_email'];
+
+			$visibility_post_author = intval(self::VISIBILITY_POST_AUTHOR);
+			$visibility_comment_author = intval(self::VISIBILITY_COMMENT_AUTHOR);
+
+
+			if($current_user_id == 0){
+				$sql = $wpdb->prepare("
+					select comment.comment_ID 
+						from {$wpdb->comments} comment
+							inner join {$wpdb->commentmeta} meta on meta.comment_id = comment.comment_ID and meta.meta_key = %s
+							left outer join {$wpdb->comments} comment_parent on comment_parent.comment_ID = comment.comment_parent
+					where comment.comment_ID in ({$comment_ids})
+						and !(comment.comment_author = %s and comment.comment_author_email = %s)
+						and ( 
+							(meta.meta_value = '{$visibility_post_author}' ) 
+							|| (meta.meta_value = '{$visibility_comment_author}' and comment_parent.user_id != {$current_user_id})
+							|| (meta.meta_value = '{$visibility_comment_author}' and !(comment_parent.comment_author = %s && comment_parent.comment_author_email = %s) ) 
+						)", self::FIELD_PREFIX . 'visibility', wp_specialchars_decode($comment_author,ENT_QUOTES), $comment_author_email, wp_specialchars_decode($comment_author,ENT_QUOTES), $comment_author_email);
+			}
+			else{
+				$sql = $wpdb->prepare("
+					select comment.comment_ID 
+						from {$wpdb->comments} comment
+							inner join {$wpdb->commentmeta} meta on meta.comment_id = comment.comment_ID and meta.meta_key = %s
+							inner join {$wpdb->posts} post on post.ID = comment.comment_post_ID 
+							left outer join {$wpdb->comments} comment_parent on comment_parent.comment_ID = comment.comment_parent
+					where comment.comment_ID in ({$comment_ids})
+						and post.post_author != {$current_user_id} 
+						and comment.user_id != {$current_user_id}
+						and !(comment.comment_author = %s and comment.comment_author_email = %s)
+						and ( 
+							(meta.meta_value = '{$visibility_post_author}' ) 
+							|| (meta.meta_value = '{$visibility_comment_author}' and comment_parent.user_id != {$current_user_id})
+							|| (meta.meta_value = '{$visibility_comment_author}' and !(comment_parent.comment_author = %s && comment_parent.comment_author_email = %s) ) 
+						)", self::FIELD_PREFIX . 'visibility', wp_specialchars_decode($comment_author,ENT_QUOTES), $comment_author_email, wp_specialchars_decode($comment_author,ENT_QUOTES), $comment_author_email);
+			}
+			
+			$removed_comments = $wpdb->get_col($sql);
+
+			while(count($removed_comments) > 0){
+				$comment_id_to_remove = array_pop($removed_comments);
+				
+				foreach($comments as $key => $comment){
+					if($comment->comment_ID == $comment_id_to_remove){
+						unset($comments[$key]);
+					}
+					else if($comment->comment_parent == $comment_id_to_remove){
+						array_push($comment->comment_ID);
+					}
+				}
+			}
+
+			$this->_comment_counts[$post_id] = count($comments);
+			return array_values($comments);
 		}
 
 		/**
@@ -98,7 +189,9 @@
 			$field_names = array_keys($this->getFields());
 
 			foreach($field_names as $field_name){
-				add_comment_meta( $comment_id, self::FIELD_PREFIX . $field_name, $_POST[$field_name] );
+				if(isset($_POST[$field_name]) && !empty($_POST[$field_name])){
+					add_comment_meta( $comment_id, self::FIELD_PREFIX . $field_name, $_POST[$field_name] );
+				}
 			}
 		}
 	}
